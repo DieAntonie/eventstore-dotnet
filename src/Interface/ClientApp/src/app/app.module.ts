6e33c015e087150aff6aba1537d8bf30a9fd8b7b
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-07-20 22:05:14 +0200

    Rename fetch-data to weather-list

--- src/Interface/ClientApp/src/app/app.module.ts
+++ src/Interface/ClientApp/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { WeatherListComponent } from './weather-list/weather-list.component';
 import { WeatherAlertComponent } from './weather-alert/weather-alert.component';
 
 @NgModule({
@@ -20,7 +20,7 @@ import { WeatherAlertComponent } from './weather-alert/weather-alert.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
+    WeatherListComponent,
     WeatherAlertComponent
   ],
   imports: [
@@ -30,7 +30,7 @@ import { WeatherAlertComponent } from './weather-alert/weather-alert.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'weather-list', component: WeatherListComponent },
     ])
   ],
   providers: [],
