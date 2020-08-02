3020150dcd4dce997dd240ae0b4363dd452ac2f0
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-08-02 13:33:26 +0200

    Add more message service logging

--- src/Interface/ClientApp/src/app/heroes/heroes.component.ts
+++ src/Interface/ClientApp/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,14 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`${this.constructor.name}.onSelect(${JSON.stringify(hero)})`);
     this.selectedHero = hero;
   }
   
